d1c5dc0a0f6c22ccab984c2ada54b9cdbfb3b64c
Yankowski <Janikto@gmail.com>
2016-05-31 07:27:10 +0200

    delete unnecessary actions in LineItemsController

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,4 +1,4 @@
 class CartController < ApplicationController
 
-  
+
 end
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -39,29 +39,6 @@ class LineItemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /line_items/1
-  # PATCH/PUT /line_items/1.json
-  def update
-    respond_to do |format|
-      if @line_item.update(line_item_params)
-        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @line_item }
-      else
-        format.html { render :edit }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /line_items/1
-  # DELETE /line_items/1.json
-  def destroy
-    @line_item.destroy
-    respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
